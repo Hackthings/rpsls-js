c4b0978fed5658c124b16a9d84dfbcc6a852544c
Theresa Casiano <Tree@Theresas-MacBook-Pro.local>
2015-01-19 15:32:11 -0800

    Added function to reset game scores (needs a button).  Added code in rpsls_game function to track player and computer scores as well as number of games, all of which are printed to the console for now.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//global functions
+var number_of_games = 0;
+var player_score = 0;
+var computer_score = 0;
+
+
 // "How to Play" Lightbox Display
 
 var $overlay = $('<div id="overlay"></div>');
@@ -33,27 +39,27 @@ $overlay.click(function() {
   
   $('#rock').click(function(){
     player_name = "rock";
-    console.log("You have chosen " + player_name + ".");
+    console.log("You have chosen ROCK.");
     rpsls_game(player_name);
   });
   $('#paper').click(function(){
     player_name = "paper";
-    console.log("You have chosen " + player_name + ".");
+    console.log("You have chosen PAPER.");
     rpsls_game(player_name);
   });
   $('#scissors').click(function(){
     player_name = "scissors";
-    console.log("You have chosen " + player_name + ".");
+    console.log("You have chosen SCISSORS.");
     rpsls_game(player_name);
   });
   $('#lizard').click(function(){
     player_name = "lizard";
-    console.log("You have chosen " + player_name + ".");
+    console.log("You have chosen LIZARD.");
     rpsls_game(player_name);
   });
   $('#spock').click(function(){
     player_name = "spock";
-    console.log("You have chosen " + player_name + ".");
+    console.log("You have chosen SPOCK.");
     rpsls_game(player_name);
   }); 
 
@@ -62,7 +68,7 @@ $overlay.click(function() {
   // convert player's choice name to a number
 
 function name_to_number(player_name){
-    player_name = player_name.toLowerCase();
+
     if(player_name === "rock") {
       player_name = 0;
       return player_name;
@@ -90,25 +96,33 @@ function number_to_name(computer_number){
     var computer_name;
     
     if (computer_number === 0) {
-      computer_name = "rock";
+      computer_name = "ROCK";
       return computer_name;
     } else if (computer_number === 1){
-        computer_name = "Spock";
+        computer_name = "SPOCK";
         return computer_name;
     } else if (computer_number === 2){
-        computer_name = "paper";
+        computer_name = "PAPER";
         return computer_name;
     } else if (computer_number === 3){
-        computer_name = "lizard";
+        computer_name = "LIZARD";
         return computer_name;
     } else if (computer_number === 4){
-        computer_name = "scissors";
+        computer_name = "SCISSORS";
         return computer_name;
     } else {
               // temporarily sending output to console (error msg)
       console.log("Error! The computer has chosen a number other than 0, 1, 2, 3, or 4. There must be something wrong with the random number generator.");      
     }
 }
+
+// reset game scores
+
+function reset(){
+    number_of_games = 0;
+    player_score = 0;
+    computer_score = 0;
+} 
 
 // GAME CODE
 // triggered by image/button click
@@ -117,7 +131,7 @@ function rpsls_game(player_name) {
     var computer_choice;
     var difference;
     var player_number;
-  
+
     // convert player's choice (player_name) to player_number using the function name_to_number()
     player_number = name_to_number(player_name); 
     
@@ -130,19 +144,31 @@ function rpsls_game(player_name) {
     // output the message for computer's choice
     console.log("Computer chooses " + computer_choice + ".");
     
-    /// compute difference between computer_number and player_number modulo 5
+    // compute difference between computer_number and player_number modulo 5
 
 // Unlike Python, JavaScript does not have a true modular operator. Instead, you have to do use this formula:  [Dividend-{Math.floor(Dividend/Divisor)*Divisor}] 
     
     difference = ((player_number-computer_number)-(Math.floor((player_number-computer_number)/5)*5));
-    
+  
+    number_of_games +=1;
+    console.log("RESULTS OF GAME #" + number_of_games + ":");
 // determine winner, output winner message
     if (difference === 1 || difference === 2) {
         console.log("Player wins!");
+        player_score += 1;
+        console.log("Player Score: " + player_score);
+        console.log("Computer Score: " + computer_score);
+        console.log("\n");
+        
     } else if (difference === 3 || difference === 4) {
         console.log("Computer wins!");
-    } else {
+        computer_score +=1;
+        console.log("Player Score: " + player_score);
+        console.log("Computer Score: " + computer_score);
+        console.log("\n");
+    } else {       
         console.log("Player and computer tie.");
+        console.log("\n");
     }
 }
 
